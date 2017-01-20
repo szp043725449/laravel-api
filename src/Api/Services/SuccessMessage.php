31ac35b85d45ba21b99d563ea7867ff784ed9bd2
sunzhiping <szp043725449@126.com>
2017-01-20 13:29:18 +0800

    v1.0.1

--- src/Api/Services/SuccessMessage.php
+++ src/Api/Services/SuccessMessage.php
@@ -7,9 +7,9 @@ use Illuminate\Http\Response;
 
 class SuccessMessage extends Message
 {
-    public function __construct()
+    public function __construct($data = [])
     {
-        parent::__construct(Response::HTTP_OK, 'ok');
+        parent::__construct(Response::HTTP_OK, 'ok', $data);
     }
 
     public function isSuccess()
